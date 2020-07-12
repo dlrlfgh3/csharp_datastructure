67fd8ded505e2d295d01c66856843df36cfeba04
이길호 <dlrlfgh3@gmail.com>
2020-07-12 14:37:06 +0900

    1. dynamicArray 클래스 만듦

---
+++ dynamicArray/dynamicArray/DynamicArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dynamicArray
+{
+    class DynamicArray<T>
+    {
+        const int _defaultArrayCount = 2;
+        int _arrCount = 0;
+        int _arrCapacity = _defaultArrayCount;
+        T[] _arr = new T[_defaultArrayCount];
+
+        public DynamicArray()
+        {
+
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return _arr[index];
+            }
+
+            set
+            {
+                if (index < 0 || index > _arr.Length - 1)
+                {
+                    throw new Exception();
+                }
+                _arr[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            _arrCount++;
+            if (_arrCapacity < _arrCount)
+            {
+                _arrCapacity *= 2;
+                T[] newArr = new T[_arrCapacity];
+                Array.Copy(_arr, newArr, _arrCount - 1);
+                _arr = newArr;
+            }
+            _arr[_arrCount - 1] = item;
+        }
+
+        public void remove(int index)
+        {
+            if (index < 0 || index > _arrCapacity)
+                throw new Exception();
+
+            for (int i = index; i < _arrCount - 1; i++)
+            {
+                _arr[index] = _arr[index + 1];
+            }
+            _arrCount--;
+        }
+    }
+}
--- dynamicArray/dynamicArray/Program.cs
+++ dynamicArray/dynamicArray/Program.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace dynamicArray
 {
+    class Temp
+    {
+        int a = 0;
+
+        public Temp(int a)
+        {
+            this.a = a;
+        }
+        
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hi");
+            DynamicArray<Temp> arr = new DynamicArray<Temp>();
+            arr.Add(new Temp(0));
+            arr.Add(new Temp(1));
+            arr.Add(new Temp(2));
+            arr.remove(2);
+            arr.Add(new Temp(3));
+            arr.Add(new Temp(4));
+
+            arr.remove(2);
         }
     }
 }
