0bca86de3e8a156e2a520e2a1e4097ac7ad65a39
이길호 <dlrlfgh3@gmail.com>
2020-07-21 23:21:38 +0900

    싱글리스트 Add함수 만듦

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class CLinkedList<T>
+    {
+        Node _head;
+        Node _tail;
+        class Node
+        {
+            public Node()
+            {
+                _nextNode = null;   
+            }
+
+            public Node(T data, ref Node head)
+            {
+                _data = data;
+                head._nextNode = this;
+                head = this;
+                _nextNode = null;
+            }
+            T _data;
+            Node _nextNode;
+        }
+
+        public CLinkedList()
+        {
+             _head = new Node();
+            _tail = _head;
+        }
+
+        public void Add(T data)
+        {
+            new Node(data, ref _head);
+        }
+
+        public void Remove()
+        {
+
+        }
+
+    }
+}
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
+    public class TT
+    {
+        public TT(int a)
+        {
+            this.a = a;
+        }
+        int a;
+    }
+
+
     class Program
     {
+
         static void Main(string[] args)
         {
-
+            CLinkedList<TT> t = new CLinkedList<TT>();
+            t.Add(new TT(1));
+            t.Add(new TT(2));
+            t.Add(new TT(3));
+            t.Add(new TT(4));
+            t.Add(new TT(5));
 
         }
     }
